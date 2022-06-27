e00b2526a9f0968f2f4a9f770acaf8c070cce4ed
codingchild <codingchild@gmail.com>
2022-06-27 06:41:00 +0000

    akt embedding tinkering

--- src/models/akt.py
+++ src/models/akt.py
@@ -37,12 +37,39 @@ class MultiheadAttention(nn.Module):
     def forward(self):
         pass
 
+# 원 코드 69~93까지
 class RaschModelEmbedding(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        num_q, #num_q: concept의 unique한 수
+        emb_size,
+        ):
         super().__init__()
+
+        self.emb_size = emb_size
+
+        # c_ct
+        self.q_emb = nn.Embedding(self.num_q, self.emb_size)
+        # (ct, rt)
+        self.qr_emb = nn.EmbeddingBag(self.num_q * 2, self.emb_size)
+        # d_ct
+        self.q_emb_diff = nn.Embedding(self.num_q, self.emb_size)
+        # difficult parameter
+        self.u_emb = nn.Embedding(self.num_pid, 1)
+
+
+    def forward(self, q, qr, pid):
+        """
+        q_data: concept
+        qr: concept + response
+        pid: problem id
+        """
+        q_emb = self.q_emb(q)
+        qr_emb = self.qr_emb(qr)
+
+
         pass
-    def forward(self):
-        pass
+        
 
 
 # AKT Main 모델
