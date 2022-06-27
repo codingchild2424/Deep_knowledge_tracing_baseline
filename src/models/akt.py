8f5d4200b8c4898d320e65c6a681de8e2c031411
codingchild <codingchild@gmail.com>
2022-06-27 05:30:06 +0000

    add akt skeleton

--- src/models/akt.py
+++ src/models/akt.py
@@ -1,10 +1,55 @@
-
+import numpy as np
 import torch.nn as nn
+import torch
+
+class MonotonicAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self):
+
+        minus_theta = -1e8
+        distance = self.distance_func()
+
+        np.exp(minus_theta * distance)
+
+        
+        s = 0
+
+        a = self.softmax(s)
+
+        return a
+
+    #grad를 갱신하지 않음
+    @torch.no_grad()
+    def distance_func(self, t, tau):
+
+        
+
+        np.absolute(t - tau)
+
+class MultiheadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    def forward(self):
+        pass
+
+class RaschModelEmbedding(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    def forward(self):
+        pass
 
 
+# AKT Main 모델
 class AKT(nn.Module):
 
     def __init__(self):
+        super().__init__()
         pass
 
     def forward(self):
